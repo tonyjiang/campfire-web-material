4497f868b83a3f148f8aa4da88935c423809e73e
Tony Jiang <yujiang99@gmail.com>
2022-06-25 17:55:45 -0700

    Now we have a structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Grid from '@mui/material/Grid'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction='column' className='App-container'>
+      <Grid container item className='App-header' xs={2} md={2}>
+        <Grid item className='upper-left' xs={3.5}>App Icon & Search Box</Grid>
+        <Grid item xs={5}>Icon Links to Main Components</Grid>
+        <Grid item xs={3.5}>Profile Icon & Notification Icon & Grid Icon</Grid>
+      </Grid>
+      <Grid container item className='App-body' xs={10} md={10}>
+        <Grid item xs={2} lg={3.5}>Left Nav</Grid>
+        <Grid item xs={8} lg={5}>Feed</Grid>
+        <Grid item xs={2} lg={3.5}>What's Happening</Grid>
+      </Grid>
+    </Grid>
   );
 }
 
