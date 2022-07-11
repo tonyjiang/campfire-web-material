427906254c93df3d980eff6228c301a3ed1282e6
Tony Jiang <yujiang99@gmail.com>
2022-07-10 23:02:54 -0700

    Add buttons for CourseEvents

--- src/components/CourseEvents.jsx
+++ src/components/CourseEvents.jsx
@@ -1,27 +1,9 @@
-import {
-  AddBox,
-  ArrowDownward,
-  ArrowUpward,
-  Cancel,
-  Check,
-  Clear,
-  Delete,
-  Edit,
-  FilterList,
-  FirstPage,
-  LastPage,
-  ChevronLeft,
-  ChevronRight,
-  Remove,
-  Search,
-} from "@mui/icons-material";
-import { Box, Skeleton, Stack, TextField } from "@mui/material";
+import { Box, Button, Skeleton, Stack, TextField } from "@mui/material";
 import MaterialTable from "material-table";
-import { forwardRef, useState } from "react";
+import { useState } from "react";
 import TableIcons from './TableIcons';
 
 const CourseEvents = (courseId, events) => {
-  const [loading, setLoading] = useState(true);
   const columns = [
     { title: "Title", field: "title" },
     { title: "Description", field: "description" },
@@ -41,46 +23,48 @@ const CourseEvents = (courseId, events) => {
 
   const [data, setData] = useState([]);
 
-  setTimeout(() => {
-    setLoading(false);
-  }, [2000]);
-
   return (
-    <MaterialTable
-      title="Edit course events"
-      columns={columns}
-      data={data}
-      icons={TableIcons}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              setData([...data, newData]);
-              resolve();
-            }, 1000);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const dataUpdate = [...data];
-              const index = oldData.tableData.id;
-              dataUpdate[index] = newData;
-              setData([...dataUpdate]);
-              resolve();
-            }, 1000);
-          }),
-        onRowDelete: (oldData) =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const dataDelete = [...data];
-              const index = oldData.tableData.id;
-              dataDelete.splice(index, 1);
-              setData([...dataDelete]);
-              resolve();
-            }, 1000);
-          }),
-      }}
-    />
+    <>
+      <MaterialTable
+        title="Edit course events"
+        columns={columns}
+        data={data}
+        icons={TableIcons}
+        editable={{
+          onRowAdd: (newData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                setData([...data, newData]);
+                resolve();
+              }, 1000);
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.id;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+                resolve();
+              }, 1000);
+            }),
+          onRowDelete: (oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataDelete = [...data];
+                const index = oldData.tableData.id;
+                dataDelete.splice(index, 1);
+                setData([...dataDelete]);
+                resolve();
+              }, 1000);
+            }),
+        }}
+      />
+      <Box sx={{ display: "flex", justifyContent: "flex-end", paddingTop: 2, paddingRight: 2 }}>
+        <Button variant="outlined" size="large" sx={{ marginRight: 2 }}>Cancel</Button>
+        <Button variant="contained" size="large">Save</Button>
+      </Box>
+    </>
   );
 };
 
