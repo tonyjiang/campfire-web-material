62771b6bdd4cb1837ea49d99cc7d89288e0c92d8
Tony Jiang <86689874+hippopotony@users.noreply.github.com>
2022-10-13 00:25:52 -0700

    Clean it up

--- src/components/post/Thread.tsx
+++ src/components/post/Thread.tsx
@@ -1,25 +1,8 @@
 import {
-  Avatar,
   Card,
-  CardHeader,
-  CardContent,
-  CardActions,
-  IconButton,
-  ImageList,
-  ImageListItem,
   List,
   ListItem,
-  styled,
-  Typography,
 } from "@mui/material";
-import { blue, red } from "@mui/material/colors";
-import {
-  ChatBubbleOutline,
-  Favorite,
-  FavoriteBorder,
-  IosShareOutlined,
-  MoreVert,
-} from "@mui/icons-material";
 
 import React, { useEffect, useState } from "react";
 import axios from "../../api/axios";
@@ -27,26 +10,16 @@ import axios from "../../api/axios";
 import CreatePost from "./CreatePost";
 import Post from "./Post";
 
-const CommentCard = styled(Card)(({ theme }) => ({
-  color: theme.palette.primary.light,
-  width: "100%",
-  marginLeft: "5px",
-}));
-
 export default function Thread(props) {
   // "op" is the original post of the thread
   const [op, setOp] = useState(props.op);
   const [comments, setComments] = useState([]);
-  const [likes, setLikes] = useState([]);
-  const [imageUrls, setImageUrls] = useState([]);
   const [error, setError] = useState();
   const cachedUser = JSON.parse(localStorage.getItem("user") || "");
 
   useEffect(() => {
     setOp(props.op);
     setComments(props.op.comments);
-    setLikes(props.op.likes || []);
-    setImageUrls(props.op.image_urls);
   }, [props]);
 
   const renderedComments = () => {
@@ -84,38 +57,6 @@ export default function Thread(props) {
       });
   };
 
-  const handleLikeClick = () => {
-    const like = likes?.find((like) => like.user_id === cachedUser.id);
-    if (like) {
-      axios
-        .delete(`/api/v1/posts/${op.id}/likes/${like.id}`)
-        .then(() => {
-          const newLikes = likes.filter((like) => like.user_id !== cachedUser.id);
-          setLikes(newLikes);
-        })
-        .catch((err) => {
-          console.error(err);
-          setError(err);
-        });
-    } else {
-      const data = {
-        user_id: 1,
-        likable_type: "Post",
-        likable_id: op.id,
-      };
-      axios
-        .post(`/api/v1/posts/${op.id}/likes`, data)
-        .then((resp) => {
-          const newLikes = [...likes, resp.data];
-          setLikes(newLikes);
-        })
-        .catch((err) => {
-          console.error(err);
-          setError(err);
-        });
-    }
-  };
-
   if (error)
     return (
       <div>
@@ -132,7 +73,7 @@ export default function Thread(props) {
         addNewPost={addNewComment}
         style={{ marginLeft: "20px" }}
       />
-      <Post post={op} isComment={false} />
+      <Post post={op} />
       { renderedComments() }
     </Card>
   );
